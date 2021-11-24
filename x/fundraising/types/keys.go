76efecae7b19d2651a92ccf2df145a7bd5f0eec6
kogisin <jaybkim2@gmail.com>
2021-11-24 22:23:54 +0900

    feat: adding store key prefixes

--- x/fundraising/types/keys.go
+++ x/fundraising/types/keys.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+)
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "fundraising"
@@ -7,16 +12,44 @@ const (
 	// StoreKey defines the primary module store key
 	StoreKey = ModuleName
 
-	// RouterKey is the message route for slashing
+	// RouterKey is the message route for the fundraising module
 	RouterKey = ModuleName
 
 	// QuerierRoute defines the module's query routing key
 	QuerierRoute = ModuleName
-
-	// MemStoreKey defines the in-memory store key
-	MemStoreKey = "mem_fundraising"
 )
 
-func KeyPrefix(p string) []byte {
-	return []byte(p)
+// keys for fundraising store prefixes
+var (
+	AuctionIdKey = []byte("0x11") // key for the latest auction id
+	SequenceKey  = []byte("0x12") // key for the latest sequence number
+
+	AuctionKeyPrefix    = []byte("0x21") // key for auction id to find an auction
+	AuctioneerKeyPrefix = []byte("0x22") // key for auctineer address to find an auction id
+
+	SequenceKeyPrefix = []byte("0x31") // key for auction id with the sequence number to find the bid
+	BidKeyPrefix      = []byte("0x32") // key for auction id with bidder address to find the sequence number
+	BidderKeyPrefix   = []byte("0x33") // key for bidder address to find the sequence number
+)
+
+// GetAuctionKey returns key/value indexing key of the auction.
+func GetAuctionKey(auctionID uint64) []byte {
+	return append(AuctionIdKey, sdk.Uint64ToBigEndian(auctionID)...)
+}
+
+// GetAuctioneerKey returns key/value indexing key of the auction.
+func GetAuctioneerKey(auctioneerAcc sdk.AccAddress) []byte {
+	return append(AuctioneerKeyPrefix, address.MustLengthPrefix(auctioneerAcc)...)
+}
+
+func GetSequenceKey(auctionID uint64, sequence uint64) []byte {
+	return append(append(SequenceKeyPrefix, sdk.Uint64ToBigEndian(auctionID)...), sdk.Uint64ToBigEndian(sequence)...)
+}
+
+func GetBidKey(auctionID uint64, auctioneerAcc sdk.AccAddress) []byte {
+	return append(append(BidKeyPrefix, sdk.Uint64ToBigEndian(auctionID)...), address.MustLengthPrefix(auctioneerAcc)...)
+}
+
+func GetBidderKey(auctioneerAcc sdk.AccAddress) []byte {
+	return append(BidderKeyPrefix, address.MustLengthPrefix(auctioneerAcc)...)
 }
