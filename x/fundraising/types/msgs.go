0dfbc12ed91b5734232e1f03c1527d3aade503de
kogisin <jaybkim2@gmail.com>
2021-11-29 15:50:28 +0900

    feat: add msg validation

--- x/fundraising/types/msgs.go
+++ x/fundraising/types/msgs.go
@@ -53,20 +53,38 @@ func (msg MsgCreateFixedPriceAuction) ValidateBasic() error {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid auctioneer address %q: %v", msg.Auctioneer, err)
 	}
 	if !msg.StartPrice.IsPositive() {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "start price must be positve value %s", msg.StartPrice)
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "start price must be positve %s", msg.StartPrice)
 	}
 	if err := msg.SellingCoin.Validate(); err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid selling coin %q: %v", msg.SellingCoin, err)
 	}
-	if err := sdk.ValidateDenom(msg.PayingCoinDenom); err != nil {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid paygin coin denom %s: %v", msg.PayingCoinDenom, err)
+	if !msg.SellingCoin.Amount.IsPositive() {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "selling coin amount must be positive %s", msg.SellingCoin)
 	}
-
+	if err := sdk.ValidateDenom(msg.PayingCoinDenom); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid paying coin denom %s: %v", msg.PayingCoinDenom, err)
+	}
 	if !msg.EndTime.After(msg.StartTime) {
 		return sdkerrors.Wrapf(ErrInvalidAuctionEndTime, "end time %s must be greater than start time %s", msg.EndTime.Format(time.RFC3339), msg.StartTime.Format(time.RFC3339))
 	}
-	// TODO: vesting schedules validation not implemented yet
 
+	// TODO: consider if we want infinite number of vesting schedules
+	if len(msg.VestingSchedules) > 0 {
+		totalWeight := sdk.ZeroDec()
+		for _, vs := range msg.VestingSchedules {
+			if !vs.Weight.IsPositive() {
+				return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "vesting weight must be positive %s", vs.Weight)
+			}
+			if vs.Weight.GT(sdk.NewDec(1)) {
+				return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "vesting weight must not greater than 1 %s", vs.Weight)
+			}
+			totalWeight = totalWeight.Add(vs.Weight)
+		}
+
+		if !totalWeight.Equal(sdk.NewDec(1)) {
+			return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "total vesting weight must be equal to 1 %s", totalWeight)
+		}
+	}
 	return nil
 }
 
@@ -120,7 +138,28 @@ func (msg MsgCreateEnglishAuction) Route() string { return RouterKey }
 func (msg MsgCreateEnglishAuction) Type() string { return TypeMsgCreateEnglishAuction }
 
 func (msg MsgCreateEnglishAuction) ValidateBasic() error {
-	// TODO: not implemented yet
+	if _, err := sdk.AccAddressFromBech32(msg.Auctioneer); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid auctioneer address %q: %v", msg.Auctioneer, err)
+	}
+	if !msg.StartPrice.IsPositive() {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "start price must be positve value %s", msg.StartPrice)
+	}
+	if err := msg.SellingCoin.Validate(); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid selling coin %q: %v", msg.SellingCoin, err)
+	}
+	if err := sdk.ValidateDenom(msg.PayingCoinDenom); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid paygin coin denom %s: %v", msg.PayingCoinDenom, err)
+	}
+	if !msg.MaximumBidPrice.IsPositive() {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "maximum bid price must be positve value %s", msg.MaximumBidPrice)
+	}
+	if !msg.ExtendRate.IsPositive() {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "extend rate must be positve value %s", msg.ExtendRate)
+	}
+	if !msg.EndTime.After(msg.StartTime) {
+		return sdkerrors.Wrapf(ErrInvalidAuctionEndTime, "end time %s must be greater than start time %s", msg.EndTime.Format(time.RFC3339), msg.StartTime.Format(time.RFC3339))
+	}
+	// TODO: vesting schedules validation not implemented yet
 	return nil
 }
 
@@ -160,7 +199,9 @@ func (msg MsgCancelFundraising) Route() string { return RouterKey }
 func (msg MsgCancelFundraising) Type() string { return TypeMsgCancelFundraising }
 
 func (msg MsgCancelFundraising) ValidateBasic() error {
-	// TODO: not implemented yet
+	if _, err := sdk.AccAddressFromBech32(msg.Auctioneer); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid auctioneer address %q: %v", msg.Auctioneer, err)
+	}
 	return nil
 }
 
@@ -204,7 +245,15 @@ func (msg MsgPlaceBid) Route() string { return RouterKey }
 func (msg MsgPlaceBid) Type() string { return TypeMsgPlaceBid }
 
 func (msg MsgPlaceBid) ValidateBasic() error {
-	// TODO: not implemented yet
+	if _, err := sdk.AccAddressFromBech32(msg.Bidder); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid bidder address %q: %v", msg.Bidder, err)
+	}
+	if !msg.Price.IsPositive() {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "bid price must be positve value %s", msg.Price)
+	}
+	if err := msg.Coin.Validate(); err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid bid coin %q: %v", msg.Coin, err)
+	}
 	return nil
 }
 
--- x/fundraising/types/msgs_test.go
+++ x/fundraising/types/msgs_test.go
@@ -1,9 +1,94 @@
 package types_test
 
-import "testing"
+import (
+	"testing"
+	time "time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/tendermint/tendermint/crypto"
+
+	"github.com/tendermint/fundraising/x/fundraising/types"
+)
 
 func TestMsgCreateFixedPriceAuction(t *testing.T) {
-	// TODO: not implemented yet
+	auctioneerAcc := sdk.AccAddress(crypto.AddressHash([]byte("Auctioneer")))
+	startTime, _ := time.Parse(time.RFC3339, "2021-11-01T22:08:41+00:00") // needs to be deterministic for test
+	endTime := startTime.AddDate(1, 0, 0)
+
+	testCases := []struct {
+		expectedErr string
+		msg         *types.MsgCreateFixedPriceAuction
+	}{
+		{
+			"", // empty means no error expected
+			types.NewMsgCreateFixedPriceAuction(
+				auctioneerAcc.String(),
+				sdk.MustNewDecFromStr("0.5"),
+				sdk.NewInt64Coin("ugdex", 10_000_000_000_000),
+				"uatom",
+				[]types.VestingSchedule{},
+				startTime,
+				endTime,
+			),
+		},
+		{
+			"start price must be positve 0.000000000000000000: invalid request",
+			types.NewMsgCreateFixedPriceAuction(
+				auctioneerAcc.String(),
+				sdk.MustNewDecFromStr("0"),
+				sdk.NewInt64Coin("ugdex", 10_000_000_000_000),
+				"uatom",
+				[]types.VestingSchedule{},
+				startTime,
+				endTime,
+			),
+		},
+		{
+			"selling coin amount must be positive 0ugdex: invalid request",
+			types.NewMsgCreateFixedPriceAuction(
+				auctioneerAcc.String(),
+				sdk.MustNewDecFromStr("0.5"),
+				sdk.NewInt64Coin("ugdex", 0),
+				"uatom",
+				[]types.VestingSchedule{},
+				startTime,
+				endTime,
+			),
+		},
+		// TODO: vesting schedules not covered
+		{
+			"end time 2020-11-01T22:08:41Z must be greater than start time 2021-11-01T22:08:41Z: invalid auction end time",
+			types.NewMsgCreateFixedPriceAuction(
+				auctioneerAcc.String(),
+				sdk.MustNewDecFromStr("0.5"),
+				sdk.NewInt64Coin("ugdex", 10_000_000_000_000),
+				"uatom",
+				[]types.VestingSchedule{},
+				startTime,
+				startTime.AddDate(-1, 0, 0),
+			),
+		},
+	}
+
+	for _, tc := range testCases {
+		require.IsType(t, &types.MsgCreateFixedPriceAuction{}, tc.msg)
+		require.Equal(t, types.TypeMsgCreateFixedPriceAuction, tc.msg.Type())
+		require.Equal(t, types.RouterKey, tc.msg.Route())
+		require.Equal(t, sdk.MustSortJSON(legacy.Cdc.MustMarshalJSON(tc.msg)), tc.msg.GetSignBytes())
+
+		err := tc.msg.ValidateBasic()
+		if tc.expectedErr == "" {
+			require.Nil(t, err)
+			signers := tc.msg.GetSigners()
+			require.Len(t, signers, 1)
+			require.Equal(t, tc.msg.GetAuctioneer(), signers[0])
+		} else {
+			require.EqualError(t, err, tc.expectedErr)
+		}
+	}
 }
 
 func TestMsgCreateEnglishAuction(t *testing.T) {
