d82585d664f27df2e9cda2af6536b831974e20f6
kogisin <jaybkim2@gmail.com>
2021-11-30 12:02:58 +0900

    test: add functions in testutil package

--- testutil/keeper/fundraising.go
+++ testutil/keeper/fundraising.go
@@ -4,35 +4,37 @@ import (
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/codec"
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
-	"github.com/tendermint/fundraising/x/fundraising/keeper"
-	"github.com/tendermint/fundraising/x/fundraising/types"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
+
+	"github.com/tendermint/fundraising/testutil/sample"
+	"github.com/tendermint/fundraising/x/fundraising/keeper"
+	"github.com/tendermint/fundraising/x/fundraising/types"
 )
 
-func FundraisingKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
+// Fundraising returns a keeper of the fundraising module for testing purpose.
+func Fundraising(t testing.TB) (*keeper.Keeper, sdk.Context) {
+	cdc := sample.Codec()
+	db := tmdb.NewMemDB()
+	stateStore := store.NewCommitMultiStore(db)
+
+	keeper := initFundraising(cdc, db, stateStore)
+	require.NoError(t, stateStore.LoadLatestVersion())
+
+	return keeper, sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
+}
+
+func initFundraising(cdc codec.Codec, db *tmdb.MemDB, stateStore store.CommitMultiStore) *keeper.Keeper {
 	storeKey := sdk.NewKVStoreKey(types.StoreKey)
 	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
 
-	db := tmdb.NewMemDB()
-	stateStore := store.NewCommitMultiStore(db)
 	stateStore.MountStoreWithDB(storeKey, sdk.StoreTypeIAVL, db)
 	stateStore.MountStoreWithDB(memStoreKey, sdk.StoreTypeMemory, nil)
-	require.NoError(t, stateStore.LoadLatestVersion())
 
-	registry := codectypes.NewInterfaceRegistry()
-	k := keeper.NewKeeper(
-		codec.NewProtoCodec(registry),
-		storeKey,
-		memStoreKey,
-	)
-
-	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
-	return k, ctx
+	return keeper.NewKeeper(cdc, storeKey, memStoreKey)
 }
--- testutil/sample/sample.go
+++ testutil/sample/sample.go
@@ -1,9 +1,29 @@
 package sample
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	fundraisingtypes "github.com/tendermint/fundraising/x/fundraising/types"
 )
+
+// Codec returns a codec with preregistered interfaces.
+func Codec() codec.Codec {
+	interfaceRegistry := codectypes.NewInterfaceRegistry()
+
+	cryptocodec.RegisterInterfaces(interfaceRegistry)
+	authtypes.RegisterInterfaces(interfaceRegistry)
+	stakingtypes.RegisterInterfaces(interfaceRegistry)
+	banktypes.RegisterInterfaces(interfaceRegistry)
+	fundraisingtypes.RegisterInterfaces(interfaceRegistry)
+
+	return codec.NewProtoCodec(interfaceRegistry)
+}
 
 // AccAddress returns a sample account address
 func AccAddress() string {
