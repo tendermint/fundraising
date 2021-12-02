302368217cf7a526c0b71f3a5e0688fd04bd6205
kogisin <jaybkim2@gmail.com>
2021-12-02 21:45:41 +0900

    feat: add skeleton for query command line interfaces

--- x/fundraising/client/cli/query.go
+++ x/fundraising/client/cli/query.go
@@ -1,12 +1,18 @@
 package cli
 
 import (
+	"context"
 	"fmt"
+	"strings"
+
 	// "strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/version"
+
 	// "github.com/cosmos/cosmos-sdk/client/flags"
 	// sdk "github.com/cosmos/cosmos-sdk/types"
 
@@ -25,6 +31,184 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(
+		QueryParams(),
+		QueryAuctions(),
+		QueryAuction(),
+		QueryBids(),
+		QueryVestings(),
+	)
+
+	return cmd
+}
+
+func QueryParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query the current fundraising parameters information",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as fundraising parameters.
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			resp, err := queryClient.Params(context.Background(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(&resp.Params)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func QueryAuctions() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "auctions",
+		Args:  cobra.NoArgs,
+		Short: "Query the current fundraising parameters information",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as fundraising parameters.
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			fmt.Println(queryClient)
+
+			// TODO: not implemented yet
+
+			// return clientCtx.PrintProto(&resp.Params)
+			return nil
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func QueryAuction() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "auction",
+		Args:  cobra.NoArgs,
+		Short: "Query the current fundraising parameters information",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as fundraising parameters.
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			fmt.Println(queryClient)
+
+			// TODO: not implemented yet
+
+			// return clientCtx.PrintProto(&resp.Params)
+			return nil
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func QueryBids() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "bids",
+		Args:  cobra.NoArgs,
+		Short: "Query the current fundraising parameters information",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as fundraising parameters.
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			fmt.Println(queryClient)
+
+			// TODO: not implemented yet
+
+			// return clientCtx.PrintProto(&resp.Params)
+			return nil
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func QueryVestings() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "vestings",
+		Args:  cobra.NoArgs,
+		Short: "Query the current fundraising parameters information",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`Query values set as fundraising parameters.
+Example:
+$ %s query %s params
+`,
+				version.AppName, types.ModuleName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+			fmt.Println(queryClient)
+
+			// TODO: not implemented yet
+
+			// return clientCtx.PrintProto(&resp.Params)
+			return nil
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
 
 	return cmd
 }
