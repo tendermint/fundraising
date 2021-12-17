b0c94613c92587bab61351745341e988802f4394
kogisin <jaybkim2@gmail.com>
2021-12-17 18:19:15 +0900

    test: add cli test file

---
+++ x/fundraising/client/testutil/cli_test.go
@@ -0,0 +1,16 @@
+//go:build norace
+// +build norace
+
+package testutil
+
+import (
+	"testing"
+)
+
+func TestTxCmdTestSuite(t *testing.T) {
+	// suite.Run(t, new(TxCmdTestSuite))
+}
+
+func TestQueryCmdTestSuite(t *testing.T) {
+	// suite.Run(t, new(QueryCmdTestSuite))
+}
