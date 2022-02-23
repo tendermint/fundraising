bc3d52c9b112baf41e3ea32ca0350c7676e8ce29
kogisin <jaybkim2@gmail.com>
2022-02-23 19:00:46 +0900

    feat: add BatchAuctionRequest struct and its parse helper function

--- x/fundraising/client/cli/utils.go
+++ x/fundraising/client/cli/utils.go
@@ -42,3 +42,38 @@ func (req FixedPriceAuctionRequest) String() string {
 	}
 	return string(result)
 }
+
+// BatchAuctionRequest defines CLI request for an batch auction.
+type BatchAuctionRequest struct {
+	StartPrice        sdk.Dec                 `json:"start_price"`
+	SellingCoin       sdk.Coin                `json:"selling_coin"`
+	PayingCoinDenom   string                  `json:"paying_coin_denom"`
+	MaxExtendedRound  uint32                  `json:"max_extended_round"`
+	ExtendedRoundRate sdk.Dec                 `json:"extended_round_rate"`
+	VestingSchedules  []types.VestingSchedule `json:"vesting_schedules"`
+	StartTime         time.Time               `json:"start_time"`
+	EndTime           time.Time               `json:"end_time"`
+}
+
+// ParseBatchAuctionRequest reads the file and parses BatchAuctionRequest.
+func ParseBatchAuctionRequest(fileName string) (req BatchAuctionRequest, err error) {
+	contents, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return req, err
+	}
+
+	if err = json.Unmarshal(contents, &req); err != nil {
+		return req, err
+	}
+
+	return req, nil
+}
+
+// String returns a human readable string representation of the request.
+func (req BatchAuctionRequest) String() string {
+	result, err := json.Marshal(&req)
+	if err != nil {
+		panic(err)
+	}
+	return string(result)
+}
--- x/fundraising/client/cli/utils_test.go
+++ x/fundraising/client/cli/utils_test.go
@@ -63,3 +63,59 @@ func TestParseFixedPriceAuction(t *testing.T) {
 	require.Equal(t, "denom2", auction.PayingCoinDenom)
 	require.EqualValues(t, expSchedules, auction.VestingSchedules)
 }
+
+func TestParseBatchAuction(t *testing.T) {
+	okJSON := testutil.WriteToNewTempFile(t, `
+{
+  "start_price": "1.000000000000000000",
+  "selling_coin": {
+    "denom": "denom1",
+    "amount": "1000000000000"
+  },
+  "paying_coin_denom": "denom2",
+  "vesting_schedules": [
+    {
+      "release_time": "2022-01-01T00:00:00Z",
+      "weight": "0.500000000000000000"
+    },
+    {
+      "release_time": "2022-06-01T00:00:00Z",
+      "weight": "0.250000000000000000"
+    },
+    {
+      "release_time": "2022-12-01T00:00:00Z",
+      "weight": "0.250000000000000000"
+    }
+  ],
+  "max_extended_round": 3,
+  "extended_round_rate": "0.200000000000000000",
+  "start_time": "2021-11-01T00:00:00Z",
+  "end_time": "2021-12-01T00:00:00Z"
+}
+`)
+
+	expSchedules := []types.VestingSchedule{
+		{
+			ReleaseTime: types.MustParseRFC3339("2022-01-01T00:00:00Z"),
+			Weight:      sdk.MustNewDecFromStr("0.50"),
+		},
+		{
+			ReleaseTime: types.MustParseRFC3339("2022-06-01T00:00:00Z"),
+			Weight:      sdk.MustNewDecFromStr("0.25"),
+		},
+		{
+			ReleaseTime: types.MustParseRFC3339("2022-12-01T00:00:00Z"),
+			Weight:      sdk.MustNewDecFromStr("0.25"),
+		},
+	}
+
+	auction, err := cli.ParseBatchAuctionRequest(okJSON.Name())
+	require.NoError(t, err)
+	require.NotEmpty(t, auction.String())
+	require.Equal(t, sdk.MustNewDecFromStr("1.0"), auction.StartPrice)
+	require.Equal(t, sdk.NewInt64Coin("denom1", 1000000000000), auction.SellingCoin)
+	require.Equal(t, "denom2", auction.PayingCoinDenom)
+	require.Equal(t, uint32(3), auction.MaxExtendedRound)
+	require.Equal(t, sdk.MustNewDecFromStr("0.2"), auction.ExtendedRoundRate)
+	require.EqualValues(t, expSchedules, auction.VestingSchedules)
+}
