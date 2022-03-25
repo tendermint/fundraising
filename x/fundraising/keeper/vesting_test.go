2ac25cd5eedf906c73c99742edccc31ca4e8f512
kogisin <jaybkim2@gmail.com>
2022-03-25 18:09:56 +0900

    feat: applying renamed functions and cleaning up related codes

--- x/fundraising/keeper/vesting_test.go
+++ x/fundraising/keeper/vesting_test.go
@@ -3,42 +3,47 @@ package keeper_test
 import (
 	"time"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-
 	"github.com/tendermint/fundraising/x/fundraising/types"
 
 	_ "github.com/stretchr/testify/suite"
 )
 
-func (s *KeeperTestSuite) TestVestingQueue_RemainingCoin() {
+func (s *KeeperTestSuite) TestApplyVestingSchedules() {
+	// TODO: Normal Vesting Schedules
+}
+
+func (s *KeeperTestSuite) TestApplyVestingSchedules_RemainingCoin() {
+	startTime := time.Now().AddDate(0, 0, -1)
+	endTime := startTime.AddDate(0, 1, 0)
+
 	auction := s.createFixedPriceAuction(
 		s.addr(0),
 		parseDec("1.0"),
-		sdk.NewInt64Coin("denom1", 1_000_000_000_000),
+		parseCoin("1_000_000_000_000denom1"),
 		"denom2",
 		[]types.VestingSchedule{
 			{
-				ReleaseTime: time.Now().AddDate(0, 0, -1).AddDate(0, 6, 0),
-				Weight:      sdk.MustNewDecFromStr("0.3"),
+				ReleaseTime: endTime.AddDate(0, 6, 0),
+				Weight:      parseDec("0.3"),
 			},
 			{
-				ReleaseTime: time.Now().AddDate(0, 0, -1).AddDate(0, 9, 0),
-				Weight:      sdk.MustNewDecFromStr("0.3"),
+				ReleaseTime: endTime.AddDate(0, 9, 0),
+				Weight:      parseDec("0.3"),
 			},
 			{
-				ReleaseTime: time.Now().AddDate(0, 0, -1).AddDate(1, 0, 0),
-				Weight:      sdk.MustNewDecFromStr("0.4"),
+				ReleaseTime: endTime.AddDate(1, 0, 0),
+				Weight:      parseDec("0.4"),
 			},
 		},
-		time.Now().AddDate(0, 0, -1),
-		time.Now().AddDate(0, 0, -1).AddDate(0, 1, 0),
+		startTime,
+		endTime,
 		true,
 	)
 	s.Require().Equal(types.AuctionStatusStarted, auction.GetStatus())
 
-	s.placeBidFixedPrice(auction.GetId(), s.addr(1), sdk.OneDec(), parseCoin("20000000denom2"), true)
-	s.placeBidFixedPrice(auction.GetId(), s.addr(2), sdk.OneDec(), parseCoin("20000000denom2"), true)
-	s.placeBidFixedPrice(auction.GetId(), s.addr(2), sdk.OneDec(), parseCoin("15000000denom2"), true)
+	s.placeBidFixedPrice(auction.GetId(), s.addr(1), parseDec("1.0"), parseCoin("20000000denom2"), true)
+	s.placeBidFixedPrice(auction.GetId(), s.addr(2), parseDec("1.0"), parseCoin("20000000denom2"), true)
+	s.placeBidFixedPrice(auction.GetId(), s.addr(2), parseDec("1.0"), parseCoin("15000000denom2"), true)
 
 	err := s.keeper.ApplyVestingSchedules(s.ctx, auction)
 	s.Require().NoError(err)
