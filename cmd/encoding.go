349f758b886df8c978d7336340de72a3d0d8ae4b
Danilo Pantani <danpantani@gmail.com>
2023-04-11 01:23:52 -0300

    rollback cosmosapp.go file

--- cmd/encoding.go
+++ cmd/encoding.go
@@ -1,60 +1,22 @@
 package cmd
 
 import (
-	abci "github.com/cometbft/cometbft/abci/types"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectype "github.com/cosmos/cosmos-sdk/codec/types"
-	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	"github.com/cosmos/cosmos-sdk/std"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 )
 
-type (
-	// CosmosApp implements the common methods for a Cosmos SDK-based application
-	// specific blockchain.
-	CosmosApp interface {
-		// Name is the assigned name of the app.
-		Name() string
-
-		// The application types codec.
-		// NOTE: This should be sealed before being returned.
-		LegacyAmino() *codec.LegacyAmino
-
-		// BeginBlocker updates every begin block.
-		BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) abci.ResponseBeginBlock
-
-		// EndBlocker updates every end block.
-		EndBlocker(ctx sdk.Context, req abci.RequestEndBlock) abci.ResponseEndBlock
-
-		// InitChainer updates at chain (i.e app) initialization.
-		InitChainer(ctx sdk.Context, req abci.RequestInitChain) abci.ResponseInitChain
-
-		// LoadHeight loads the app at a given height.
-		LoadHeight(height int64) error
-
-		// ExportAppStateAndValidators exports the state of the application for a genesis file.
-		ExportAppStateAndValidators(
-			forZeroHeight bool,
-			jailAllowedAddrs []string,
-			modulesToExport []string,
-		) (servertypes.ExportedApp, error)
-
-		// ModuleAccountAddrs are registered module account addreses.
-		ModuleAccountAddrs() map[string]bool
-	}
-
-	// EncodingConfig specifies the concrete encoding types to use for a given app.
-	// This is provided for compatibility between protobuf and amino implementations.
-	EncodingConfig struct {
-		InterfaceRegistry codectype.InterfaceRegistry
-		Marshaler         codec.Codec
-		TxConfig          client.TxConfig
-		Amino             *codec.LegacyAmino
-	}
-)
+// EncodingConfig specifies the concrete encoding types to use for a given app.
+// This is provided for compatibility between protobuf and amino implementations.
+type EncodingConfig struct {
+	InterfaceRegistry codectype.InterfaceRegistry
+	Marshaler         codec.Codec
+	TxConfig          client.TxConfig
+	Amino             *codec.LegacyAmino
+}
 
 // makeEncodingConfig creates an EncodingConfig for an amino based test configuration.
 func makeEncodingConfig() EncodingConfig {
