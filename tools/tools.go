462e870ef5afb86f9541a45753dfb1c8d45a014e
Danilo Pantani <danpantani@gmail.com>
2023-04-27 02:23:31 -0300

    add missing tools and migrate config

---
+++ tools/tools.go
@@ -0,0 +1,21 @@
+//go:build tools
+// +build tools
+
+package tools
+
+// Manage tool dependencies via go.mod.
+//
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+// https://github.com/golang/go/issues/25922
+
+//nolint:all
+import (
+	_ "github.com/cosmos/gogoproto/protoc-gen-gocosmos"
+	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "mvdan.cc/gofumpt"
+)
